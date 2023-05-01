a9ff87dded7bea202964e429f130dd330d0a2ec2
Manuel Seiche <hakenadu91@gmail.com>
2023-05-01 13:24:57 +0200

    docker-compose build column-forge-api + nginx config column-forge-ui

--- column-forge-ui/src/environments/environment.ts
+++ column-forge-ui/src/environments/environment.ts
@@ -1,4 +1,13 @@
-export const environment = {
+import {InjectionToken} from '@angular/core';
+
+export interface Environment {
+  production: boolean;
+  apiUrl: string;
+}
+
+export const ENVIRONMENT = new InjectionToken('the ui configuration used for the current runtime environment');
+
+export const environment: Environment = {
   production: false,
   apiUrl: 'http://localhost:8080'
 };
--- column-forge-ui/src/main.ts
+++ column-forge-ui/src/main.ts
@@ -1,7 +1,28 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
+import {ENVIRONMENT, environment, Environment} from './environments/environment';
+import {enableProdMode} from '@angular/core';
 
+async function main() {
+  let env: Environment;
+  try {
+    const data = await fetch('/environment.json');
+    env = await data.json();
+  } catch (error) {
+    console.warn('no environment.json read => falling back to default')
+    env = environment;
+  }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  if (env.production) {
+    enableProdMode();
+  }
+
+  await platformBrowserDynamic([
+    {provide: ENVIRONMENT, useValue: env}
+  ]).bootstrapModule(AppModule)
+    .then(() => console.log(env))
+    .catch(err => console.error(err));
+}
+
+main();
